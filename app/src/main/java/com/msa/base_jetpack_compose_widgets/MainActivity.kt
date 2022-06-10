0628c539bd21e906f0b02c0bcb50f57a40129a65
ZARMACARON\a-soleymani <Solimaniali90@gmail.com>
2022-06-10 10:21:23 +0430

    first commit

---
+++ app/src/main/java/com/msa/base_jetpack_compose_widgets/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.msa.base_jetpack_compose_widgets
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.msa.base_jetpack_compose_widgets.ui.theme.Base_JetPack_Compose_WidgetsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Base_JetPack_Compose_WidgetsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+  Text(text = name)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Base_JetPack_Compose_WidgetsTheme {
+        Greeting("Android")
+    }
+}
+
---
+++ table_section1/src/main/java/com/msa/table_section1/Person.kt
@@ -0,0 +1,9 @@
+package com.msa.table_section1
+
+data class Person(
+    var name:String,
+    var age:Int,
+    var hasDrivingLicence:Boolean,
+    var email:String
+
+)
